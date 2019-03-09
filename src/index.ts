c3a9101b7795ad8e65dfb94e12052c87d2ba206d
jpontife <jpontife@sfu.ca>
2019-03-08 20:40:23 -0800

    Added more starter code

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
 import { App } from './app';
+import { importSchema } from 'graphql-import';
+import ping from './resolvers/ping';
+import * as path from 'path';
 
-const Server = new App();
+const resolvers = {
+  Query: {
+    ping,
+  },
+};
+
+const typeDefs = importSchema(path.join(__dirname, '../schema.graphql'));
+
+const Server = new App(resolvers, typeDefs);
+Server.start();
